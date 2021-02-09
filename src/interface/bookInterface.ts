e2eb7010529ef5e3c70d5bf015e11097f1ad4cbd
mofed salah <mofed.salah.hana@gmail.com>
2021-02-09 01:19:40 -0800

    project structure

--- src/interface/bookInterface.ts
+++
@@ -1,6 +0,0 @@
-interface userCrud {
-  find(req: any, res: any): void;
-  create(req: any, res: any): void;
-  update(req: any, res: any): void;
-  delete(req: any, res: any): void;
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.set("port", process.env.PORT || 3000);
-
-console.log("Hello");
-
-export default app;
